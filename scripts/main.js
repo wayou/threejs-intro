3705be283ff0ea5eb9f560491451d03bf81df1ed
wayou <liuwayong@gmail.com>
2014-10-30 15:36:08 +0800

    improve

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,27 @@
-   var scene, camera, renderer;
-   var geometry, material, mesh;
-   init();
-   animate();
+ $(function() {
+     $.deck('.slide');
+     var scene, camera, renderer;
+     var geometry, material, mesh;
+     init();
+     animate();
 
-   function init() {
-       scene = new THREE.Scene();
-       camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
-       camera.position.z = 1000;
-       geometry = new THREE.BoxGeometry(200, 200, 200);
-       material = new THREE.MeshNormalMaterial();
-       mesh = new THREE.Mesh(geometry, material);
-       scene.add(mesh);
-       renderer = new THREE.WebGLRenderer();
-       renderer.setSize(window.innerWidth, window.innerHeight);
-       document.body.appendChild(renderer.domElement);
-   }
+     function init() {
+         scene = new THREE.Scene();
+         camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+         camera.position.z = 1000;
+         geometry = new THREE.BoxGeometry(200, 200, 200);
+         material = new THREE.MeshNormalMaterial();
+         mesh = new THREE.Mesh(geometry, material);
+         scene.add(mesh);
+         renderer = new THREE.WebGLRenderer();
+         renderer.setSize(window.innerWidth, window.innerHeight);
+         document.getElementById('slide-0').appendChild(renderer.domElement);
+     }
 
-   function animate() {
-       requestAnimationFrame(animate);
-       mesh.rotation.x += 0.01;
-       mesh.rotation.y += 0.02;
-       renderer.render(scene, camera);
-   }
+     function animate() {
+         requestAnimationFrame(animate);
+         mesh.rotation.x += 0.01;
+         mesh.rotation.y += 0.02;
+         renderer.render(scene, camera);
+     }
+ });
